716c42127607d756d16b8b9af5263c4a37c523af
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2022-12-05 12:39:55 +0100

    add python-dotenv

--- database/db_config.py
+++ database/db_config.py
@@ -1,7 +1,11 @@
 import os
 
+from dotenv import load_dotenv
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
 
 
 db_engine = create_async_engine(os.environ["DATABASE_URL"], future=True, echo=True)
