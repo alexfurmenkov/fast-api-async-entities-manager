0da52990500ea023b4c95e2163fc66f6b7298be0
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2023-01-31 13:06:29 +0100

    add unique username constraint to users table

--- database/db_models/user_model.py
+++ database/db_models/user_model.py
@@ -1,10 +1,11 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, UniqueConstraint
 
 from .base_model import BaseModel
 
 
 class UserDBModel(BaseModel):
     __tablename__ = "users"
+    __table_args__ = (UniqueConstraint('username', name='_username_unique_constraint'), )
 
     id = Column(String, primary_key=True)
     username = Column(String, nullable=False)
