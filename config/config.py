cf12f82d433d2b3dc88fa4395a1353caec569943
alexfurmenkov <alexeyfurmenkov@gmail.com>
2024-03-25 13:08:29 +0100

    freeze requirements versions

--- config/config.py
+++ config/config.py
@@ -1,14 +1,14 @@
 import os
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
 
 class AppConfig(BaseSettings):
     DATABASE_URL: str = os.environ["DATABASE_URL"]
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.environ["ACCESS_TOKEN_EXPIRE_MINUTES"])
-    REFRESH_TOKEN_EXPIRE_MINUTES: int = int(os.environ["REFRESH_TOKEN_EXPIRE_MINUTES"])
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", 43800))  # one month
     JWT_SECRET_KEY: str = os.environ["JWT_SECRET_KEY"]
     JWT_ALGORITHM: str = os.environ["JWT_ALGORITHM"]
